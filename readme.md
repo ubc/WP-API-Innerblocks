41dac7ea8a62286c6a71f5eb1787ae32ef972d67
Kelvin Xu <kelvin.xu@ubc.ca>
2024-11-25 09:53:28 -0800

    add error handling that stop WP_HTML_Tag_Processor from processing if pagination is not rendering.

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 Contributors:      Kelvin Xu
 Tags:              block
 Tested up to:      6.5
-Stable tag:        1.0.0
+Stable tag:        1.0.2
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
--- ubc-wp-api-inner-blocks.php
+++ ubc-wp-api-inner-blocks.php
@@ -4,7 +4,7 @@
  * Description:       Provide inner blocks that consumes data from external API.
  * Requires at least: 6.5
  * Requires PHP:      8.2
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Kelvin Xu
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
