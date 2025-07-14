1072cb72f9c589420155c7232fa998d55d92c3ae
Kelvin Xu <kelvin.xu@ubc.ca>
2025-07-14 11:50:14 -0700

    Update plugin version

--- class-apiinnerblocks.php
+++ class-apiinnerblocks.php
@@ -4,7 +4,7 @@
  * Description:       Provide inner blocks that consumes data from external API.
  * Requires at least: 6.5
  * Requires PHP:      8.2
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            Kelvin Xu
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
